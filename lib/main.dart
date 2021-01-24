b8841d72d4812ef6d63ba646a99e75c655566bdc
Ishaan Karnik <ishaankarnik88@gmail.com>
2021-01-24 16:00:57 +0530

    Chapter 3 Complete

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:brew_crew/screens/wrapper.dart';
+import 'package:flutter/material.dart';
+import 'package:brew_crew/screens/wrapper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
+}
